b3c4af26060a893ce1931aab9d01b9cfaa6059e0
Pedro Soria <psorialopez84@gmail.com>
2020-05-09 07:33:42 -0500

    Parte 4 - Notificaciones

--- app/src/main/java/com/example/miparte4/ServicioMusica.java
+++ app/src/main/java/com/example/miparte4/ServicioMusica.java
@@ -1,13 +1,23 @@
 package com.example.miparte4;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+
 public class ServicioMusica extends Service {
     MediaPlayer reproductor;
+
+    private static final int ID_NOTIFICACION_CREAR = 1;
+    public static final String NOTIFICATION_CHANNEL_ID = "1000";
+    public static final String NOTIFICATION_CHANNEL_NAME = "MICANAL";
 
     @Override
     public void onCreate() {
@@ -17,6 +27,24 @@ public class ServicioMusica extends Service {
 
     @Override
     public int onStartCommand(Intent intenc, int flags, int idArranque) {
+        NotificationCompat.Builder notific = new NotificationCompat.Builder(this)
+                .setContentTitle("Creando Servicio de Música")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("información adicional");
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(
+                            NOTIFICATION_CHANNEL_ID,
+                            NOTIFICATION_CHANNEL_NAME,
+                            NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(R.color.colorAccent);
+            notificationManager.createNotificationChannel(notificationChannel);
+            notific.setChannelId(NOTIFICATION_CHANNEL_ID);
+        }
+        notificationManager.notify(ID_NOTIFICACION_CREAR, notific.build());
         Toast.makeText(this,"Servicio arrancado "+ idArranque, Toast.LENGTH_SHORT).show();
         reproductor.start();
         return START_STICKY;
