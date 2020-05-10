19ffd9f26fa7f07c074da20908f72c8e6175c113
Pedro Soria <psorialopez84@gmail.com>
2020-05-09 19:52:41 -0500

    Parte 4 - Activando pantalla aun bloqueada - Puedo ir a AnuncioPersonalizado aunque el celular este bloqueado.

--- app/src/main/java/com/example/miparte4/AnuncioPersonalizado.java
+++ app/src/main/java/com/example/miparte4/AnuncioPersonalizado.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class AnuncioPersonalizado extends AppCompatActivity {
 
@@ -15,6 +16,12 @@ public class AnuncioPersonalizado extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        setContentView(R.layout.activity_servicio);
+
         setContentView(R.layout.activity_anuncio_personalizado);
         intentFilter = new IntentFilter("RESPONSE");
         intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
