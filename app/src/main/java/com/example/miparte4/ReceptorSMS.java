96e8492292a271c53796015a8b1789126d37b33a
Pedro Soria <psorialopez84@gmail.com>
2020-05-09 19:20:24 -0500

    Parte 4 - Receptor de Anuncios - Ejemplo 2

---
+++ app/src/main/java/com/example/miparte4/ReceptorSMS.java
@@ -0,0 +1,15 @@
+package com.example.miparte4;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ReceptorSMS extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, Servicio.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
